57cd1676d583bf4ab5ed78a6d4c41b59b8b0b4e9
ishev <sunucatmsk@gmail.com>
2024-04-07 13:24:54 +0300

    Change url for record start.

--- router/egress.go
+++ router/egress.go
@@ -4,7 +4,7 @@ import "github.com/gin-gonic/gin"
 import "lkrouter/controler"
 
 func EgressRouter(r *gin.Engine) {
-	egreessRouter := r.Group("/egress")
+	egreessRouter := r.Group("/record")
 	{
 		egreessRouter.POST("/start/", func(c *gin.Context) {
 			controler.EgressController(c)
