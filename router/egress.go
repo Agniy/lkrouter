13a2452481d67c409393076cf6fb734bb526bc2c
ishev <ilya@decard.me>
2024-04-18 10:22:30 +0300

    Add Option request.

--- router/egress.go
+++ router/egress.go
@@ -6,6 +6,15 @@ import "lkrouter/controler"
 func EgressRouter(r *gin.Engine) {
 	egreessRouter := r.Group("/record")
 	{
+		egreessRouter.OPTIONS("/start/", func(c *gin.Context) {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+			c.Writer.Header().Set("Access-Control-Allow-Methods", "POST")
+			c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+			c.Writer.Header().Set("Access-Control-Max-Age", "86400")
+			c.Writer.Header().Set("Content-Length", "0")
+			c.Writer.WriteHeader(204)
+		})
+
 		egreessRouter.POST("/start/", func(c *gin.Context) {
 			controler.EgressController(c)
 		})
